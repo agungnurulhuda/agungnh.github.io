c40ed8d43d9e90a1fec44cc270ba1fcb42ce49ba
agungnurulhuda <agungnurulhuda212@gmail.com>
2023-12-25 16:24:21 +0700

    update-11

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,7 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2500,
+    duration: 2000,
     delay: 400,
 })
 
@@ -64,5 +64,10 @@ sr.reveal(`.profile__profession`, { delay: 600 })
 sr.reveal(`.profile__social`, { delay: 700 })
 sr.reveal(`.profile__info-group`, { interval: 100, delay: 700 })
 sr.reveal(`.profile__buttons`, { delay: 800 })
-sr.reveal(`.filters__content`, { delay: 900 })
-sr.reveal(`.filters`, { delay: 1000 })
+// sr.reveal(`.filters__content`, { delay: 900 })
+// sr.reveal(`.filters`, { delay: 1000 })
+sr.reveal(`.filters__content`, { delay: 500 })
+sr.reveal(`.filters`, { delay: 500 })
+sr.reveal(`.filters__1`, { delay: 600 })
+sr.reveal(`.filters__2`, { delay: 700 })
+sr.reveal(`.filters__3`, { delay: 800 })
